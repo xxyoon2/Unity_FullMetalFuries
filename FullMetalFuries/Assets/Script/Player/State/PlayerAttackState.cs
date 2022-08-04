3546d395962eba1f6b4f8fb1d1d86ee9f60cfd77
Park Doil <103083213+xxyoon2@users.noreply.github.com>
2022-08-04 17:27:57 +0900

    [FIX] 스킬 이동속도 수정
    #구현
    - 스킬을 써도 이동키 입력을 하면 이동이 가능했으나, move speed 값을 스킬마다 변경해주면서 스킬마다 이동 속도를 다르게 줌

--- FullMetalFuries/Assets/Script/Player/State/PlayerAttackState.cs
+++ FullMetalFuries/Assets/Script/Player/State/PlayerAttackState.cs
@@ -11,6 +11,6 @@ public class PlayerAttackState : StateMachineBehaviour
     
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       animator.GetComponent<Player>().MoveSpeed = 10f;
+       animator.GetComponent<Player>().MoveSpeed = 20f;
     }
 }
--- FullMetalFuries/Assets/Script/Player/State/PlayerSpecialAttackState.cs
+++ FullMetalFuries/Assets/Script/Player/State/PlayerSpecialAttackState.cs
@@ -2,21 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDodgeState : StateMachineBehaviour
+public class PlayerSpecialAttackState : StateMachineBehaviour
 {
-    PlayerSkill pSkill;
-    Player player;
-    
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.GetComponent<Player>().MoveSpeed = 40f;
+      animator.GetComponent<Player>().MoveSpeed = 0f;
     }
-
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-       Debug.Log("damage += 10");
-    }
-
+    
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
        animator.GetComponent<Player>().MoveSpeed = 20f;
